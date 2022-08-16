54277b7035e5d9ad52d8144fd1ac4ed06c541e94
Doan Trung Hieu <dthieu1302@gmail.com>
2022-08-16 22:34:15 +0700

    feat: Update Shipspace and order ships

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,7 @@
 import { ref, getCurrentInstance, onMounted } from "vue";
 import { vue3dLoader } from "vue-3d-loader";
 
-const activeIndex = ref("1");
 const { proxy } = getCurrentInstance();
-const handleSelect = (key, keyPath) => {
-  console.log(key, keyPath);
-};
 let activeMenu = ref("home");
 const scrollTo = (name) => {
   const component = proxy.$el.querySelector(`#${activeMenu.value}`);
@@ -22,20 +18,60 @@ onMounted(() => {
   scrollTo();
 });
 
+
 /**
  * Xử lý các vấn đề view tàu
  */
-var ships = [{Id: 1, Name: "Zeus X", SubName: "Legend", Class: "ship-alpha", Path3D: "/src/assets/images/3d_model/ship_zeus/ship_zeus.gltf", Description: "The most modern generation of spaceships. Named after the King of the gods and ruler of Mount Olympus; god of the sky, lightning, thunder, law, order and justice. Crafted by Earth Protection Organization (EPO). Equipped with the best weapons and armor, able to fight the most powerful enemies." , Mass: 500, Weight: 450, Speed: 16.6, Health: 200, Attach: 200, Shield: 200},
-{Id: 2, Name: "Poseidon VII", SubName: "Epic", Class: "ship-beta", Path3D: "/src/assets/images/3d_model/ship_poisedon/ship_poisedon.gltf", Description: "The 7th generation spaceship. Named after the god of the seas. Equipped with supersonic jet engines and powerful weapons, it is easy to fight large numbers of enemies." , Mass: 600, Weight: 350, Speed: 11.2, Health: 150, Attach: 180, Shield: 180},
+var ships = [
+{Id: 1, Name: "Apollo I", SubName: "Common", Class: "ship-apollo", Path3D: "/src/assets/images/3d_model/ship_apollo/ship_apollo.gltf", Description: "The 3rd generation spacecraft was built by the Earth Defense Organization. Named after the goddess of wisdom. This is a generation of spaceships that are improved in engines and weapons compared to the first generation. Spaceships are mass-produced to protect the earth." , Mass: 800, Weight: 250, Speed: 7.9, Health: 100, Attach: 100, Shield: 100},
+{Id: 2, Name: "Athena III", SubName: "Uncommon", Class: "ship-delta", Path3D: "/src/assets/images/3d_model/ship_athena/ship_athena.gltf", Description: "The 3rd generation spacecraft was built by the Earth Defense Organization. Named after the goddess of wisdom. This is a generation of spaceships that are improved in engines and weapons compared to the first generation. Spaceships are mass-produced to protect the earth." , Mass: 800, Weight: 250, Speed: 7.9, Health: 120, Attach: 150, Shield: 100},
 {Id: 3, Name: "Ares V", SubName: "Rare", Class: "ship-gamma", Path3D: "/src/assets/images/3d_model/ship_ares/ship_ares.gltf", Description: "Equipped with medium firepower. But this is a generation of spacecraft built for special missions. Named after the god of war, this spaceship is a nightmare for enemies across the galaxy." , Mass: 700, Weight: 300, Speed: 11.2, Health: 150, Attach: 150, Shield: 150},
-{Id: 4, Name: "Athena III", SubName: "Uncommon", Class: "ship-delta", Path3D: "/src/assets/images/3d_model/ship_athena/ship_athena.gltf", Description: "The 3rd generation spacecraft was built by the Earth Defense Organization. Named after the goddess of wisdom. This is a generation of spaceships that are improved in engines and weapons compared to the first generation. Spaceships are mass-produced to protect the earth." , Mass: 800, Weight: 250, Speed: 7.9, Health: 120, Attach: 150, Shield: 100},
-{Id: 5, Name: "Apollo I", SubName: "Common", Class: "ship-apollo", Path3D: "/src/assets/images/3d_model/ship_apollo/ship_apollo.gltf", Description: "The 3rd generation spacecraft was built by the Earth Defense Organization. Named after the goddess of wisdom. This is a generation of spaceships that are improved in engines and weapons compared to the first generation. Spaceships are mass-produced to protect the earth." , Mass: 800, Weight: 250, Speed: 7.9, Health: 100, Attach: 100, Shield: 100}];
+{Id: 4, Name: "Poseidon VII", SubName: "Epic", Class: "ship-beta", Path3D: "/src/assets/images/3d_model/ship_poisedon/ship_poisedon.gltf", Description: "The 7th generation spaceship. Named after the god of the seas. Equipped with supersonic jet engines and powerful weapons, it is easy to fight large numbers of enemies." , Mass: 600, Weight: 350, Speed: 11.2, Health: 150, Attach: 180, Shield: 180},
+{Id: 5, Name: "Zeus X", SubName: "Legend", Class: "ship-alpha", Path3D: "/src/assets/images/3d_model/ship_zeus/ship_zeus.gltf", Description: "The most modern generation of spaceships. Named after the King of the gods and ruler of Mount Olympus; god of the sky, lightning, thunder, law, order and justice. Crafted by Earth Protection Organization (EPO). Equipped with the best weapons and armor, able to fight the most powerful enemies." , Mass: 500, Weight: 450, Speed: 16.6, Health: 200, Attach: 200, Shield: 200}
+];
 var shipActive = ref(ships[0]);
-
+const position = ref();
+position.value = [
+  { x: 0, y: 0, z: 0 },
+  { x: 100, y: 100, z: 100 },
+];
+const rotation = ref();
+rotation.value = [
+  { x: 0, y: 0, z: 0 },
+  { x: 10, y: 1, z: 1 },
+];
+const lights = [
+  {
+    type: "AmbientLight",
+    color: "#fff",
+  },
+  {
+    type: "DirectionalLight",
+    position: { x: 50, y: 5, z: 50 },
+    color: "#fff",
+    intensity: 1,
+  },
+  {
+    type: "PointLight",
+    color: "#fff",
+    position: { x: 100, y: -100, z: 50 },
+    intensity: 1
+  },
+  {
+    type: "HemisphereLight",
+    skyColor: "#fff",
+    groundColor: "#000000",
+    position: { x: 200, y: -200, z: 100 }
+  }
+];
 const onClickShip = (event, ship) => {
   shipActive.value = ship;
 };
 
+
+/**
+ * Partner
+ */
 var partners = [{Id: 1, Name: 'Binance', Class: "partner-binance"},
 {Id: 2, Name: 'Unity', Class: "partner-unity"},
 {Id: 3, Name: 'CoinMarketCap', Class: "partner-coinmarketcap"},
@@ -58,7 +94,7 @@ var partners = [{Id: 1, Name: 'Binance', Class: "partner-binance"},
           data-bs-target="#navbarResponsive"
           aria-controls="navbarResponsive"
           aria-label="Toggle navigation"
-          aria-expanded="false"> Menu <span class="navbar-toggler-icon"></span> </button>
+          aria-expanded="false" > Menu <span class="navbar-toggler-icon"></span> </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav">
             <li class="nav-item mx-0 mx-lg-1">
@@ -215,10 +251,12 @@ var partners = [{Id: 1, Name: 'Binance', Class: "partner-binance"},
                     <vue3dLoader
                       :height="350"
                       :filePath="shipActive.Path3D"
-                      backgroundColor="rgba(255, 255, 255, 0.2)"
-                      :backgroundAlpha="0.2"
-                      :camera-position="{x: 0,y:0,z: 0}"
-                      :rotation="{x:0.5, y:-0.5, z:0}"
+                      backgroundColor="rgba(255, 255, 255)"
+                      :backgroundAlpha="0.25"
+                      :camera-position="{x: 0, y: 0, z: 0}"
+                      :rotation="rotation"
+                      :position="position"
+                      :lights="lights"
                     ></vue3dLoader>
                   </div>
                   
